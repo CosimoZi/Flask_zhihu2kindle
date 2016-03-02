13007149c624223de7b2b420e46a73f4117a18cb
CosimoZi <xzyufo@gmail.com>
2016-03-03 04:47:34 +0800

    使用服务器默认的邮件服务

--- config.py
+++ config.py
@@ -7,12 +7,6 @@ class Config:
     SECRET_KEY = 'hard to guess string'
     DEBUG = True
 
-    MAIL_SERVER='smtp-mail.outlook.com'
-    MAIL_PORT=465
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'zhihu2kindle@outlook.com'
-    MAIL_PASSWORD = 'afcaerca1fava'
-
     @staticmethod
     def init_app(app):
         pass
